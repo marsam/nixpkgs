3b755de2915b70180520727fcc4c5bf9f0e925c5
Mario Rodas <marsam@users.noreply.github.com>
2019-01-22 08:43:16 -0500

    [wip] kythe: 0.0.28 -> 0.0.29

--- pkgs/development/tools/kythe/default.nix
+++ pkgs/development/tools/kythe/default.nix
@@ -1,45 +1,44 @@
-{ stdenv, binutils , fetchurl, glibc, ncurses5 }:
+{ stdenv, binutils, fetchurl, ncurses5 }:
 
 stdenv.mkDerivation rec {
-  version = "0.0.28";
-  name = "kythe-${version}";
+  pname = "kythe";
+  version = "0.0.30";
 
   src = fetchurl {
-    url = "https://github.com/google/kythe/releases/download/v0.0.28/kythe-v0.0.28.tar.gz";
-    sha256 = "1qc7cngpxw66m3krpr5x50ns7gb3bpv2bdfzpb5afl12qp0mi6zm";
+    url = "https://github.com/kythe/kythe/releases/download/v${version}/kythe-v${version}.tar.gz";
+    sha256 = "12bwhqkxfbkh3mm4wfvqflwhmbzpmlhlfykdpy6h7p9ih9ky8w6r";
   };
 
-  buildInputs =
-    [ binutils ];
+  buildInputs = [ binutils ];
 
   doCheck = false;
 
   dontBuild = true;
 
   installPhase = ''
-    cd tools
-    for exe in http_server \
-                kythe read_entries triples verifier \
-                write_entries write_tables entrystream; do
-      echo "Patching:" $exe
+    mkdir -p $out/share/kythe
+    cp -R * $out/share/kythe
+    ln -s $out/share/kythe/tools $out/bin
+  '';
+
+  postFixup = ''
+    for exe in $out/bin/*; do
       patchelf --interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" $exe
-      patchelf --set-rpath "${stdenv.cc.cc.lib}/lib64:${ncurses5}/lib" $exe
+      patchelf --set-rpath "${stdenv.lib.makeLibraryPath [ stdenv.cc.cc.lib ncurses5 ]}" $exe
     done
-    cd ../
-    cp -R ./ $out
-    ln -s $out/tools $out/bin
   '';
 
   meta = with stdenv.lib; {
-    description = "A pluggable, (mostly) language-agnostic ecosystem for building tools that work with code.";
+    description = "A pluggable, (mostly) language-agnostic ecosystem for building tools that work with code";
     longDescription = ''
-    The Kythe project was founded to provide and support tools and standards
+      The Kythe project was founded to provide and support tools and standards
       that encourage interoperability among programs that manipulate source
       code. At a high level, the main goal of Kythe is to provide a standard,
       language-agnostic interchange mechanism, allowing tools that operate on
       source code — including build systems, compilers, interpreters, static
       analyses, editors, code-review applications, and more — to share
-      information with each other smoothly.  '';
+      information with each other smoothly.
+    '';
     homepage = https://kythe.io/;
     license = licenses.asl20;
     platforms = platforms.linux;
