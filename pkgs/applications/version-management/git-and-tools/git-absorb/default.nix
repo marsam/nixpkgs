abea0806cdecb03204179cd49f0ae708a584d2e9
Mario Rodas <marsam@users.noreply.github.com>
2019-03-08 02:14:43 -0500

    gitAndTools.git-absorb: 0.3.0 -> 2019-02-12

--- pkgs/applications/version-management/git-and-tools/git-absorb/default.nix
+++ pkgs/applications/version-management/git-and-tools/git-absorb/default.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   name = "git-absorb-${version}";
-  version = "0.3.0";
+  version = "2019-02-12";
 
   src = fetchFromGitHub {
     owner  = "tummychow";
     repo   = "git-absorb";
-    rev    = "refs/tags/${version}";
-    sha256 = "1dm442lyk7f44bshm2ajync5pzdwvdc5xfpw2lkvjzxflmh5572z";
+    rev    = "d39efe92e6e6166495973a2b63268b44be94c51c";
+    sha256 = "0055ij1zvnxqq1dbcj6l4iabq7dfkmkvdcyc9qqxwcldsxj6mxx5";
   };
 
   buildInputs = stdenv.lib.optionals stdenv.isDarwin [ libiconv Security ];
@@ -16,9 +16,9 @@ rustPlatform.buildRustPackage rec {
   cargoSha256 = "0fvxs09b9x38vp0psvlvbj09myxrhabp95pp3nz7nxsgr7fxflrr";
 
   meta = with stdenv.lib; {
-    inherit (src.meta) homepage;
     description = "git commit --fixup, but automatic";
-    license = [ licenses.bsd3 ];
+    homepage = https://github.com/tummychow/git-absorb;
+    license = licenses.bsd3;
     maintainers = [ maintainers.marsam ];
   };
 }
