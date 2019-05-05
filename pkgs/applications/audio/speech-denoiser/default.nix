cd514de42df2a587c93813acd8c6e8965c9b4b3d
Mario Rodas <marsam@users.noreply.github.com>
2019-05-04 22:05:07 -0500

    wip

--- pkgs/applications/audio/speech-denoiser/default.nix
+++ pkgs/applications/audio/speech-denoiser/default.nix
@@ -1,27 +1,35 @@
-{ stdenv, fetchFromGitHub, pkgconfig, autoconf, automake, libtool, lv2, meson, ninja }:
+{ stdenv, fetchFromGitHub, autoreconfHook, pkgconfig, lv2, meson, ninja }:
 
-stdenv.mkDerivation  rec {
-  pname = "speech-denoiser";
-  version = "unstable-07-10-2019";
-
-  src = fetchFromGitHub {
+let
+  speech-denoiser-src = fetchFromGitHub {
     owner = "lucianodato";
-    repo = pname;
+    repo = "speech-denoiser";
     rev = "04cfba929630404f8d4f4ca5bac8d9b09a99152f";
     sha256 = "189l6lz8sz5vr6bjyzgcsrvksl1w6crqsg0q65r94b5yjsmjnpr4";
   };
 
-  nativeBuildInputs = [ pkgconfig autoconf automake libtool meson ninja ];
-  buildInputs = [ lv2 ];
+  rnnoise-nu = stdenv.mkDerivation rec {
+    pname = "rnnoise-nu";
+    version = "unstable-07-10-2019";
+    src = speech-denoiser-src;
+    sourceRoot = "source/rnnoise";
+    nativeBuildInputs = [ autoreconfHook ];
+    configureFlags = [ "--disable-examples" "--disable-doc" "--disable-shared" "--enable-static" ];
+    installTargets = [ "install-rnnoise-nu" ];
+  };
+in
+stdenv.mkDerivation  rec {
+  pname = "speech-denoiser";
+  version = "unstable-07-10-2019";
 
-  preConfigure =''
-    cd rnnoise
-    ./autogen.sh
-    mv ../ltmain.sh ./ && ./autogen.sh #This is weird but otherwise it won't work (Related to bug #24 in rnnoise) 
-    CFLAGS="-fvisibility=hidden -fPIC -Wl,--exclude-libs,ALL" \
-    ./configure --disable-examples --disable-doc --disable-shared --enable-static
-    make
-    cd ..
+  src = speech-denoiser-src;
+
+  nativeBuildInputs = [ pkgconfig meson ninja ];
+  buildInputs = [ lv2 rnnoise-nu ];
+
+  postPatch = ''
+    substituteInPlace meson.build \
+      --replace "cc.find_library('rnnoise-nu',dirs: meson.current_source_dir() + '/rnnoise/.libs/',required : true)" "cc.find_library('rnnoise-nu', required : true)"
   '';
 
   meta = with stdenv.lib; {
