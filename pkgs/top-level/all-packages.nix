07c973b0e26e46942699978e71c56f21339625b9
Roman Volosatovs <rvolosatovs@riseup.net>
2019-09-16 23:39:25 +0200

    all-packages: use go 1.12 for tendermint

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20796,7 +20796,9 @@ in
 
   telepathy-idle = callPackage ../applications/networking/instant-messengers/telepathy/idle {};
 
-  tendermint = callPackage ../tools/networking/tendermint {};
+  tendermint = callPackage ../tools/networking/tendermint {
+    buildGoModule = buildGo112Module;
+  };
 
   termdown = (newScope pythonPackages) ../applications/misc/termdown { };
 
