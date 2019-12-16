b065db168d34db7717e1e74ee2acbd9ffca84661
Mario Rodas <marsam@users.noreply.github.com>
2019-12-16 16:20:00 -0500

    fixup! guix: init at 1.0.0

--- pkgs/development/guix/guix.nix
+++ pkgs/development/guix/guix.nix
@@ -1,42 +1,44 @@
-{stdenv, fetchurl}:
-stdenv.mkDerivation rec
-  { name = "guix-${version}";
-    version = "1.0.0";
+{ stdenv, fetchurl }:
 
-    src = fetchurl {
-      url = "https://ftp.gnu.org/gnu/guix/guix-binary-${version}.${stdenv.targetPlatform.system}.tar.xz";
-      sha256 = {
-        "x86_64-linux" = "11y9nnicd3ah8dhi51mfrjmi8ahxgvx1mhpjvsvdzaz07iq56333";
-        "i686-linux" = "14qkz12nsw0cm673jqx0q6ls4m2bsig022iqr0rblpfrgzx20f0i";
-        "aarch64-linux" = "0qzlpvdkiwz4w08xvwlqdhz35mjfmf1v3q8mv7fy09bk0y3cwzqs";
-        }."${stdenv.targetPlatform.system}";
-    };
-    sourceRoot = ".";
+stdenv.mkDerivation rec {
+  pname = "guix";
+  version = "1.0.1";
 
-    outputs = [ "out" "store" "var" ];
-    phases = [ "unpackPhase" "installPhase" ];
+  src = fetchurl {
+    url = "mirror://gnu/guix/guix-binary-${version}.${stdenv.targetPlatform.system}.tar.xz";
+    sha256 = {
+      "x86_64-linux" = "0fsq12cwgv8v77slprslwcmsyhdm9hr5dcadgwfmgawz4084c98b";
+      "i686-linux" = "0iri5j26n3gjh8sih49maz7f67w993lfaqhvx6k6zwz8y3bg7xb1";
+      "aarch64-linux" = "1pkyfky12a3pbghrhi5yr6bininxzkiqqfvsry44dgdwv0zgirxf";
+    }."${stdenv.targetPlatform.system}";
+  };
 
-    installPhase = ''
-      # copy the /gnu/store content
-      mkdir -p $store
-      cp -r gnu $store
+  sourceRoot = ".";
 
-      # copy /var content
-      mkdir -p $var
-      cp -r var $var
+  outputs = [ "out" "store" "var" ];
 
-      # link guix binaries
-      mkdir -p $out/bin
-      ln -s /var/guix/profiles/per-user/root/current-guix/bin/guix $out/bin/guix
-      ln -s /var/guix/profiles/per-user/root/current-guix/bin/guix-daemon $out/bin/guix-daemon
-    '';
+  phases = [ "unpackPhase" "installPhase" ];
 
-    meta = with stdenv.lib; {
-      description = "The GNU Guix package manager";
-      homepage = https://www.gnu.org/software/guix/;
-      license = licenses.gpl3Plus;
-      maintainers = [ maintainers.johnazoidberg ];
-      platforms = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
-    };
+  installPhase = ''
+    # copy the /gnu/store content
+    mkdir -p $store
+    cp -r gnu $store
 
-  }
+    # copy /var content
+    mkdir -p $var
+    cp -r var $var
+
+    # link guix binaries
+    mkdir -p $out/bin
+    ln -s /var/guix/profiles/per-user/root/current-guix/bin/guix $out/bin/guix
+    ln -s /var/guix/profiles/per-user/root/current-guix/bin/guix-daemon $out/bin/guix-daemon
+  '';
+
+  meta = with stdenv.lib; {
+    description = "The GNU Guix package manager";
+    homepage = "https://guix.gnu.org/";
+    license = licenses.gpl3Plus;
+    maintainers = [ maintainers.johnazoidberg ];
+    platforms = [ "aarch64-linux" "i686-linux" "x86_64-linux" ];
+  };
+}
